1ae736c1e3dc519133fbd0b73ae7693dddf2328a
kaushal kishore <kaushalkishorersmt@gmail.com>
2017-03-14 13:07:44 +0530

    Implemented Technologies model

--- db/seeds.rb
+++ db/seeds.rb
@@ -44,3 +44,18 @@ end
 end
 
 puts "9 portfolio items created"
+
+#3.times do |technology|
+#  Technology.create!(
+#    name: "Technology #{technology}",
+#    portfolio_id: Portfolio.last.id
+#  )
+#end
+
+3.times do |technology|
+  Portfolio.last.technologies.create!(
+    name: "Technology #{technology}"
+  )
+end
+
+puts "3 technologies created"
