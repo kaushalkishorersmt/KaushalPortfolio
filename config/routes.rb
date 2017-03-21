c75a69fd8d1c7e6373c62f9177fb43b762c6eed1
kaushal kishore <kaushalkishorersmt@gmail.com>
2017-03-21 17:56:50 +0530

    Implemented twitter call on tech new page

--- config/routes.rb
+++ config/routes.rb
@@ -6,21 +6,15 @@ Rails.application.routes.draw do
   get 'angular-items', to: 'portfolios#angular'
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
-  #get 'pages/home'
-  #get 'pages/about'
-  #get 'pages/contact'
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
+  get 'tech-news', to: 'pages#tech_news'
 
   resources :blogs do
     member do
       get :toggle_status
     end
   end
-
-  get 'about', to: 'pages#about'
-  get 'contact', to: 'pages#contact'
-
-  #resources :blogs
-
 
   root to: 'pages#home'
 end
