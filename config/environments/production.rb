a13e466b54bfbc1a374caca6e8626ebff988014b
kaushal kishore <kaushalkishorersmt@gmail.com>
2017-03-27 12:16:04 +0530

    Integrated production calls for action cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_cable.allowed_request_origins = ['https://kaushal-portfolio.herokuapp.com', 'http://kaushal-portfolio.herokuapp.com']
+  config.action_cable.url = "wss://kaushal-portfolio.herokuapp.com/cable"
 end
